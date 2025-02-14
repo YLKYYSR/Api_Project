a0dbf8a97cc7ee1e1a01918b4448901aedea0e45
ylkyy <ylkyysr@163.com>
2025-02-14 14:40:57 +0800

    优化接口

--- src/main/java/com/sparkle/start_project/Service/impl/ProductionServiceImpl.java
+++ src/main/java/com/sparkle/start_project/Service/impl/ProductionServiceImpl.java
@@ -1,11 +1,10 @@
 package com.sparkle.start_project.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.sparkle.start_project.Domain.entity.Production;
 import com.sparkle.start_project.mapper.ProductionMapper;
-import com.sparkle.start_project.service.IProductionService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -16,6 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
  * @since 2025-01-27
  */
 @Service
-public class ProductionServiceImpl extends ServiceImpl<ProductionMapper, Production> implements IProductionService {
+public class ProductionServiceImpl extends ServiceImpl<ProductionMapper, Production> implements IService<Production> {
 
 }
--- src/test/java/com/sparkle/start_project/Service/impl/StartProjectApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.sparkle.start_project.Service.impl;
-
-import com.sparkle.start_project.StartProjectApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = StartProjectApplication.class)
-class StartProjectApplicationTests {
-
-}
